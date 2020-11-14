5cc87f2dd58f4ae50ea93133201033e7ba0107fa
satoshi-sk <kusakabe3104@gmail.com>
2020-11-14 19:27:04 +0900

    モデル単体テストコード

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,106 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  describe 'ユーザー新規登録' do
+    context '新規登録がうまくいくとき' do
+      it "nicknameとemail,passwordとpassword_confirmation,last_nameとfirst_name,last_name_kanaとfirst_name_kana,birthdayが存在すれば登録できる" do
+       expect(@user).to be_valid
+      end
+      it "passwordが6文字以上の半角英数字両方含めてあれば登録できる" do
+       @user.password = "123abc"
+       @user.password_confirmation = "123abc"
+       expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録がうまくいかないとき' do
+      it "nicknameが空だと登録できない" do
+        @user.nickname = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it 'emailが空では登録できない' do
+        @user.email = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it '重複したemailが存在する場合登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+      it 'passwordが空では登録できない' do
+        @user.password = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'passwordが５文字以下では登録できない' do
+        @user.password = "aa000"
+        @user.password_confirmation = "aa000"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+      it 'passwordが半角英数両方含まれていなければ登録できない' do
+        @user.password = "123456"
+        @user.password_confirmation = "123456"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password include both letters and numbers")
+      end
+      it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+        @user.password_confirmation = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'last_nameが空では登録できない' do
+        @user.last_name = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
+      end
+      it 'last_nameが半角では登録できない' do
+        @user.last_name = "hankaku"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name Full-width characters")
+      end
+      it 'first_nameが空では登録できない' do
+        @user.first_name = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+      it 'first_nameが半角では登録できない' do
+        @user.first_name = "hankaku"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name Full-width characters")
+      end
+      it 'last_name_kanaが空では登録できない' do
+        @user.last_name_kana = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+      end
+      it 'last_name_kanaが半角では登録できない' do
+        @user.last_name_kana = "hankaku"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana Full-width katakana characters")
+      end
+      it 'first_name_kanaが空では登録できない' do
+        @user.first_name_kana = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
+      end
+      it 'first_name_kanaが半角では登録できない' do
+        @user.first_name_kana = "hankaku"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana Full-width katakana characters")
+      end
+      it 'birthdayが空では登録できない' do
+        @user.birthday = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
+      end
+    end
+  end
+end
