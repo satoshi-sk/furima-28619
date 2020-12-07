e6265f387fdb8b891855d5e406f637faaf6734db
satoshi-sk <kusakabe3104@gmail.com>
2020-12-07 21:34:00 +0900

    editアクション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all
@@ -19,15 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -40,4 +41,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :image, :product_name, :text, :category_id, :product_condition_id, :shipping_price_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
